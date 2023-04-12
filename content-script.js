7bd40cc70fdc5b97397decfd8362131eb816f9f9
shawn.wang <shawn.wang@bigcommerce.com>
2023-04-12 16:29:49 +1000

    removed unnecessary inject script

--- content-script.js
+++ content-script.js
@@ -1,34 +1,72 @@
 console.log("Hello from your Chrome extension!");
-console.log("wait ...");
 
-let script_injected = false;
-
-function injectScript(file_path, tag) {
-    console.log(file_path);
-    var node = document.getElementsByTagName(tag)[0];
-    var script = document.createElement('script');
-    script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', file_path);
-    node.appendChild(script);
-    console.log("injected, ready to send requests");
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
-const handleFromWeb = async (event) => {
-    // log window event
-};
+function getPrimaryCart() {
+    console.log('get primary cart');
 
-window.addEventListener('message', handleFromWeb);
+    fetch("/api/storefront/carts/", {
+        "method": "GET"
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    if (msg.key == 'inject-script') {
-        if (!script_injected) {
-            injectScript(chrome.runtime.getURL('inject.js'), 'body');
-            script_injected = true;
-            sendResponse({message: 'injected'});
-        } else {
-            sendResponse({message: 'already injected before, no need to inject again'});
-        }
-    }
+function getCheckout(checkoutId) {
+    console.log('get primary checkout');
 
-    window.postMessage({ from: 'content-script.js', key: 'test', data: null });
-});
+    fetch(`/api/storefront/checkout/${checkoutId}?include=consignments.availableShippingOptions,cart.lineItems.physicalItems.options,cart.lineItems.digitalItems.options,customer,promotions.banners`, {
+        "method": "GET"
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+function addItemsToCart(cartId, items) {
+    console.log(`add items <${items}> to cart <${cartId}>`);
+
+    fetch(`/api/storefront/carts/${cartId}/items`, {
+        "headers": {
+            'Content-Type': 'application/json',
+            'x-xsrf-token': getCookie('XSRF-TOKEN'),
+        },
+        "method": "POST",
+        "body": `${items}`
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+function updateCheckoutMessage(cartId, message) {
+    console.log(`update checkout <${cartId}> message to <${message}>`);
+
+    fetch(`/api/storefront/checkouts/${cartId}`, {
+        "headers": {
+            'Content-Type': 'application/json',
+            'x-xsrf-token': getCookie('XSRF-TOKEN'),
+        },
+        "method": "PUT",
+        "body": `${message}`
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+function updateCheckoutMessage(cartId, message) {
+    console.log(`update checkout <${cartId}> message to <${message}>`);
+
+    fetch(`/api/storefront/checkouts/${cartId}`, {
+        "headers": {
+            'Content-Type': 'application/json',
+            'x-xsrf-token': getCookie('XSRF-TOKEN'),
+        },
+        "method": "PUT",
+        "body": `${message}`
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
