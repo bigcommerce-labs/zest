4df6de537698077305fdfad8f2c60ca395f5a4db
Animesh Singh <animeshsingh1987@gmail.com>
2023-05-31 09:48:40 +1000

    feat(cart): Add cart creation (#4)
    * feat(cart): Add cart creation
    
    * feat(cart): Update png

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,26 @@
 /*global chrome*/
 console.log('This is a popup!');
 import jsonview from '@pgrabovets/json-view';
+import { actions } from '../constants';
+
+const elementIds = {
+    getCartButton: 'action-get-primary-cart',
+    createCartForm: 'create-cart-form',
+    additionalActionsPanel: 'additional-actions',
+};
+
+const chromeTabsParams = { active: true, currentWindow: true };
+
+function renderJsonTree(data) {
+    const additionalActionsPanel = document.getElementById(elementIds.additionalActionsPanel);
+    document.querySelector('.json-container').innerHTML = '';
+    const tree = jsonview.create(data);
+    jsonview.render(tree, document.querySelector('.json-container'));
+    additionalActionsPanel.style.display = 'block';
+}
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    console.log(tabs);
     const currentTab = tabs[0];
-
-    console.log('current tab');
 
     chrome.runtime.sendMessage({ action: 'inject-content-script', tabId: currentTab.id }).then((response) => {
         console.log(response.msg);
@@ -14,24 +28,46 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('what is going on');
-    const element = document.getElementById('action-get-primary-cart');
-    element.addEventListener('click', () => {
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            console.log('clicked');
-            const currentTab = tabs[0];
+    const createCartForm = document.getElementById(elementIds.createCartForm);
 
-            console.log(currentTab, element);
+    createCartForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-            chrome.tabs.sendMessage(currentTab.id, { action: 'get-primary-cart' }).then((response) => {
-                console.log(response);
+        const formData = new FormData(e.target);
+        const { productId } = Object.fromEntries(formData);
+        console.log('productId is: ', productId);
 
-                // clear
-                document.querySelector('.json-container').innerHTML = '';
+        if (!productId && productId.length === 0) {
+            alert(`Please enter a product id, example '103'`);
+            return;
+        }
 
-                const tree = jsonview.create(response.json);
-                jsonview.render(tree, document.querySelector('.json-container'));
+        chrome.tabs.query(chromeTabsParams, (tabs) => {
+            console.log('tabs are', tabs);
+
+            const { id } = tabs[0];
+
+            chrome.tabs.sendMessage(id, { action: actions.createCart, productId }).then((response) => {
+                renderJsonTree(JSON.stringify(response));
+
+                createCartForm.style.display = 'none';
             });
+        });
+    });
+
+    // fetch cart on extension load
+    console.log('fetching cart');
+    chrome.tabs.query(chromeTabsParams, function (tabs) {
+        const currentTab = tabs[0];
+
+        chrome.tabs.sendMessage(currentTab.id, { action: actions.getCart }).then((response) => {
+            if (response.length === 0) {
+                createCartForm.style.display = 'block';
+
+                return;
+            }
+
+            renderJsonTree(JSON.stringify(response[0]));
         });
     });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,29 @@
 /*global chrome*/
 console.log('Hello from your zest extension!');
 
+import { actions } from '../constants';
+
+import { createPrimaryCart } from './create-cart';
 import { getPrimaryCart } from './get-cart';
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     (async () => {
-        if (request.action === 'get-primary-cart') {
+        if (request.action === actions.getCart) {
             let response = await getPrimaryCart();
             let json = await response.json();
 
-            console.log(json);
-            sendResponse({ json: JSON.stringify(json) });
+            sendResponse(json);
+        }
+
+        if (request.action === actions.createCart) {
+            console.log('request to create cart', request.productId);
+            let response = await createPrimaryCart(request.productId);
+
+            window.location.reload();
+            sendResponse(response);
         }
     })();
+
     return true;
 });
 
