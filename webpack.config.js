c34615e2553c27f7d20f8e1742cfaf22d23f41ce
Animesh Singh <animeshsingh1987@gmail.com>
2023-06-05 16:26:22 +1000

    chore(common): add styles to extension (#5)
    * feat(common): add tailwind css
    
    * feat(common): add loading styles
    
    * feat(common): fix loading styles for cart creation

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 const config = {
-  entry: {
-    main: "./src/index.js",
-    popup: "./popup/popup.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: "BCApp Toolkit",
-      myPageHeader: "Zest",
-      template: "./popup/index.html",
-      filename: "./index.html",
-      chunks: ["popup"],
-      scriptLoading: "blocking",
-    }),
-  ],
+    entry: {
+        main: './src/index.js',
+        popup: './popup/popup.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'popup'),
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'Zest',
+            myPageHeader: 'Checkout Toolkit',
+            template: './popup/index.html',
+            filename: './index.html',
+            chunks: ['popup'],
+            scriptLoading: 'blocking',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
 };
 
 module.exports = (env, argv) => {
-  const { mode } = argv;
-  config.mode = mode;
+    const { mode } = argv;
+    config.mode = mode;
 
-  if (argv.mode === "development") {
-    config.devtool = "source-map";
-    config.watch = true;
-  }
+    if (argv.mode === 'development') {
+        config.devtool = 'source-map';
+        config.watch = true;
+    }
 
-  return config;
+    return config;
 };
